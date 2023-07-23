30ceb24249e473276962482e751fd0f8cfe6507a
Xinwdoubleo <128118523+Xinwdoubleo@users.noreply.github.com>
2023-07-23 21:15:55 +0900

    [Silver V] Title: 덩치, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -8,9 +8,9 @@ Rank = []
 for i in range(n):
     rank = 1
     for j in range(n):
-        if (List[i] == List[j]) or (List[i][0] < List[j][0] and List[i][1] > List[j][1]) or (List[i][0] > List[j][0] and List[i][1] < List[j][1]):
-            continue
-        elif List[i][0] < List[j][0] and List[i][1] < List[j][1]: # 자신이 더 작을 경우,
+        if List[i][0] < List[j][0] and List[i][1] < List[j][1]: # 자신이 더 작을 경우,
             rank +=1
+        else:
+            continue
     Rank.append(rank)
 print(*Rank)
